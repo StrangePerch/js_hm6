7d71eefca106e25eea300926052274ec28df1f82
StrangePerch <sasha.aav.sv@gmail.com>
2021-06-15 20:50:43 +0300

    save

--- Task 1/code.js
+++ Task 1/code.js
@@ -17,5 +17,30 @@
     }
 }
 
-let pm = new PrintMachine(30, "red", "Times new Roman");
-pm.print("Some text");
+function GetInOuts(id, in_n, out_n) {
+    id = id.replaceAll(" ", "_");
+    let inputs = [];
+    for (let i = 0; i < in_n; i++) {
+        inputs.push(document.body.querySelector(`#${id}_input${i + 1}`));
+    }
+
+    if(out_n === 0) return inputs;
+
+    let outputs = [];
+    for (let i = 0; i < out_n; i++) {
+        outputs.push(document.body.querySelector(`#${id}_output${i + 1}`));
+    }
+    return {inputs: inputs, outputs: outputs};
+}
+
+
+function Print() {
+    let inputs = GetInOuts("Print", 4, 0);
+    let size = inputs[1].value;
+    let color = inputs[2].value
+    let family = inputs[3].value;
+    if (size !== "" && color !== "" && family !== "") {
+    let pm = new PrintMachine(size, color, family);
+    pm.print(inputs[0].value);
+    }
+}
